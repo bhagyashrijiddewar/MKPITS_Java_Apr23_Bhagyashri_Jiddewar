97cef08ccb32fd09c4cf3fdcdda3db510c442309
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-02 18:22:37 +0530

    Addition of 2 numbers by using method in one class and call thethat method in main class (Two different classes)

---
+++ JAVA/programs/AdditionArithmaticOperatior.java
@@ -0,0 +1,39 @@
+ class AdditionArithmaticOperatior{
+	
+
+public int additionIntiger(int number1, int number2){
+
+  return number1 + number2; 
+}
+
+public float additionFloat(float number3,float number4){
+   
+   return number3 + number4 ;
+} 
+
+public short additionShort(short number1,short number2){
+   
+   return  (short)(number1 + number2) ;
+} 
+
+public double additionDouble(double number5,double number6){
+   
+   return number5 + number6 ;
+} 
+public long additionLong(long number7,long number8){
+   
+   return number7 + number8 ;
+} 
+
+  // public static void main(String[] args){
+     //int number1=10,number2=15;
+    // AdditionArithmaticOperatior addition=new AdditionArithmaticOperatior();
+	// System.out.println( "Addition of 2 number :  "+ number1 +" + "+ number2 +" = " + addition.additionIntiger(number1,number2));
+	 
+
+	  //sum=addition.additionIntiger(120,150);
+      //System.out.println( "Addition of 2-numbers= "+ sum );
+   
+//}
+
+}
---
+++ JAVA/programs/MainAddition.java
@@ -0,0 +1,23 @@
+ class MainAddition{
+
+public static void main(String[] args){
+	
+	//======================================================Addition of two numbers======================================================
+int number1=250,number2=126;
+float number3=1.5f, number4=6.5f;
+double number5=12.5, number6=45.6;
+long number7=12345, number8=236584;
+short number9=1,number10=5;
+     AdditionArithmaticOperatior addition=new AdditionArithmaticOperatior();
+	 System.out.println( "Addition of 2 integer number :  "+ number1 +" + "+ number2 +" = " + addition.additionIntiger(number1,number2));
+	 System.out.println( "Addition of 2 float number :  "+ number3 +" + "+ number4 +" = " + addition.additionFloat(number3,number4));
+	 System.out.println( "Addition of 2 double number :  "+ number5 +" + "+ number6 +" = " + addition.additionDouble(number5,number6));
+	 System.out.println( "Addition of 2 long number :  "+ number7 +" + "+ number8 +" = " + addition.additionLong(number7,number8));
+	 System.out.println( "Addition of 2 short number :  "+ number9 +" + "+ number10 +" = " + addition.additionShort(number9,number10));
+
+      //System.out.println( "Addition of 2-numbers="+ number1 +" + "+ number2 +" = " + addition.additionIntiger(120,150));
+	  //System.out.println( "Addition of 2-numbers= "+ addition.additionIntiger(120,150));
+
+   
+}
+}
