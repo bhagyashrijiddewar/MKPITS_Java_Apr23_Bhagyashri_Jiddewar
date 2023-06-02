49c62a203b6f0be207446fc17755ae18c0ca615f
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-02 18:41:16 +0530

    Addition of 2 number by using 2 variables

---
+++ JAVA/programs/AdditionArithmaticOperatior1.java
@@ -0,0 +1,39 @@
+ class AdditionArithmaticOperatior1{
+	
+
+public int additionIntiger(int number1, int number2){
+
+  return number1 + number2; 
+}
+
+public float additionFloat(float number1,float number2){
+   
+   return number1 + number2 ;
+} 
+
+public short additionShort(short number1,short number2){
+   
+   return  (short)(number1 + number2) ;
+} 
+
+public double additionDouble(double number1,double number2){
+   
+   return number1 + number2 ;
+} 
+public long additionLong(long number1,long number2){
+   
+   return number1 + number2 ;
+} 
+
+  // public static void main(String[] args){
+     //int number1=10,number2=15;
+    // AdditionArithmaticOperatior addition=new AdditionArithmaticOperatior();
+	// System.out.println( "Addition of 2 number :  "+ number1 +" + "+ number2 +" = " + addition.additionIntiger(number1,number2));
+	 
+
+	  //sum=addition.additionIntiger(120,150);
+      //System.out.println( "Addition of 2-numbers= "+ sum );
+   
+//}
+
+}
