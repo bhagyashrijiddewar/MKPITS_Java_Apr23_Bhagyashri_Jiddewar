0e11cda549a7a92972dbb89d6099ab5617c6b81c
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-04 17:03:03 +0530

    Multiplication of 2 numbers by using 2 variables

---
+++ JAVA/programs/MainMultiplication.java
@@ -0,0 +1,18 @@
+ class MainMultiplication{
+
+public static void main(String[] args){
+	
+
+	
+	//======================================================Multiplication of two numbers======================================================
+
+     MultiplicationArithmaticOperatior multiple=new MultiplicationArithmaticOperatior();
+	 System.out.println( "Multiplication of 2 integer number :  "+ " = " + multiple.multipleIntiger(150,120));
+	 System.out.println( "Multiplication of 2 float number :  "+ " = " + multiple.multipleFloat(8.5f,4.8f));
+	// System.out.println( "Multiplication of 2 short number :  "+ " = " + multiple.multipleShort(4,2));
+	 System.out.println( "Multiplication of 2 double number :  "+" = " + multiple.multipleDouble(54.5,42.3));
+	 System.out.println( "Multiplication of 2 long number :  "+" = " + multiple.multipleLong(154654,13255));
+
+	  
+}
+}
---
+++ JAVA/programs/MultiplicationArithmaticOperatior.java
@@ -0,0 +1,30 @@
+ class MultiplicationArithmaticOperatior{
+	
+
+public int multipleIntiger(int number1, int number2){
+
+  return number1 * number2; 
+}
+
+public float multipleFloat(float number1,float number2){
+   
+   return number1 * number2 ;
+} 
+
+public short multipleShort(short number1,short number2){
+   
+   return  (short)(number1 * number2) ;
+} 
+
+public double multipleDouble(double number1,double number2){
+   
+   return number1 * number2 ;
+} 
+public long multipleLong(long number1,long number2){
+   
+   return number1 * number2 ;
+} 
+
+
+
+}
