657439f10f950be40be3f733c9fb6662465394a0
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-04 17:15:47 +0530

    Division of 2 numbers by using 2 variables

---
+++ JAVA/programs/DivisionArithmaticOperatior.java
@@ -0,0 +1,30 @@
+ class DivisionArithmaticOperatior{
+	
+
+public int divideIntiger(int number1, int number2){
+
+  return number1 / number2; 
+}
+
+public float divideFloat(float number1,float number2){
+   
+   return number1 / number2 ;
+} 
+
+public short divideShort(short number1,short number2){
+   
+   return  (short)(number1 / number2) ;
+} 
+
+public double divideDouble(double number1,double number2){
+   
+   return number1 / number2 ;
+} 
+public long divideLong(long number1,long number2){
+   
+   return number1 / number2 ;
+} 
+
+
+
+}
---
+++ JAVA/programs/MainDivision.java
@@ -0,0 +1,18 @@
+ class MainDivision{
+
+public static void main(String[] args){
+	
+
+	
+	//======================================================Division of two numbers======================================================
+
+     DivisionArithmaticOperatior divide=new DivisionArithmaticOperatior();
+	 System.out.println( "Division of 2 integer number :  "+ " = " + divide.divideIntiger(150,120));
+	 System.out.println( "Division of 2 float number :  "+ " = " + divide.divideFloat(8.5f,4.8f));
+	// System.out.println( "Division of 2 short number :  "+ " = " + divide.divideShort(4,2));
+	 System.out.println( "Division of 2 double number :  "+" = " + divide.divideDouble(54.5,42.3));
+	 System.out.println( "Division of 2 long number :  "+" = " + divide.divideLong(154654,13255));
+
+	  
+}
+}
