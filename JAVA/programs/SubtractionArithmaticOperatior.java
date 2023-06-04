bdf43a1977510ce0bb9b7882aa0d425f27023d42
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-04 17:05:56 +0530

    Addition of 2 numbers by using 2 variables

---
+++ JAVA/programs/SubtractionArithmaticOperatior.java
@@ -0,0 +1,30 @@
+ class SubtractionArithmaticOperatior{
+	
+
+public int subtractIntiger(int number1, int number2){
+
+  return number1 - number2; 
+}
+
+public float subtractFloat(float number1,float number2){
+   
+   return number1 - number2 ;
+} 
+
+public short subtractShort(short number1,short number2){
+   
+   return  (short)(number1 - number2) ;
+} 
+
+public double subtractDouble(double number1,double number2){
+   
+   return number1 - number2 ;
+} 
+public long subtractLong(long number1,long number2){
+   
+   return number1 - number2 ;
+} 
+
+
+
+}
