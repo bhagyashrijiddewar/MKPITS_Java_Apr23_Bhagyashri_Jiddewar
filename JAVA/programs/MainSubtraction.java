51d3c01238f8d04e29f776c4945bb054599ff35b
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-04 16:49:07 +0530

    Substraction of 2 numbers

---
+++ JAVA/programs/MainSubtraction.java
@@ -0,0 +1,20 @@
+ class MainSubtraction{
+
+public static void main(String[] args){
+	
+
+	
+	//======================================================Substraction+16 of two numbers======================================================
+
+     SubtractionArithmaticOperatior subtract=new SubtractionArithmaticOperatior();
+	 System.out.println( "Substraction of 2 integer number :  "+ " = " + subtract.subtractIntiger(150,120));
+	 System.out.println( "Substraction of 2 float number :  "+ " = " + subtract.subtractFloat(8.5f,4.8f));
+	// System.out.println( "Substraction of 2 short number :  "+ " = " + subtract.subtractShort(4,2));
+	 System.out.println( "Substraction of 2 double number :  "+" = " + subtract.subtractDouble(54.5,42.3));
+	 System.out.println( "Substraction of 2 long number :  "+" = " + subtract.subtractLong(154654,13255));
+
+      //System.out.println( "Addition of 2-numbers="+ number1 +" + "+ number2 +" = " + addition.additionIntiger(120,150));
+	  //System.out.println( "Addition of 2-numbers= "+ addition.additionIntiger(120,150));
+	  
+}
+}
