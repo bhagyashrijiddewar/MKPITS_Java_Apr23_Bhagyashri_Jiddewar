fea8be7897c79652ca188d0dbc76a7a5c7d24007
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-16 11:19:13 +0530

    Inheritance example: class bottle

---
+++ JAVA/programs/bottle.java
@@ -0,0 +1,27 @@
+class bottle{
+  Double height;
+  Double Weight;
+  String bottlecolor;
+  Double BottleCapacity; // 1 ltr,2 ltr,----
+ 
+}
+class Plastic extends bottle{
+String BottleId;
+Sring Brand;
+MaterialType materialType;
+String MaterialQuality;
+
+}
+class Metal extends bottle{
+	Sring Brand;
+	String BottleId;
+	MaterialType materialType;
+
+}
+class Glass extends bottle{
+	Sring Brand;
+    String BottleId;
+	Double GlassWidth;
+}
+
+
