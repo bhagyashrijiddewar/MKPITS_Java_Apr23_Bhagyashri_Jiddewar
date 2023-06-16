f86cba69e6e942df4e40c67d06937952c33512ba
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-16 14:03:18 +0530

    Inhritance example: class Drinks

--- JAVA/programs/Drinks.java
+++ JAVA/programs/Drinks.java
@@ -1,5 +1,5 @@
 class Drinks{
-	String Taste;
+	String Taste; //Sugar,Sugar free
 	String Color;
 	string Consistancy;
 }
@@ -12,22 +12,22 @@ class Juice extends SoftDrinks{
     String Ingradients; // sugar, black salt, chat masala---
 }
 class MangoJuice extends Juice{
-	    MangoType mangoType; //Alfanso mango,Dashehri mango,Ratnagiri mango,kesar mango,----
-		String DrinksFlavour;
-		String Ingradients;
-	    String DrinksFlavour; 
+	MangoType mangoType; //Alfanso mango,Dashehri mango,Ratnagiri mango,kesar mango,----
+	String DrinksFlavour;
+	String Ingradients;
+    String DrinksFlavour; 
 }
 class OrangeJuice extends Juice{
-	    OrangeType orangeType; //Nagpuri Orange,bloodorange,Common Orange,Acidless orange,----
-		String DrinksFlavour;
-		String Ingradients;
-	    String DrinksFlavour; 
+	OrangeType orangeType; //Nagpuri Orange,bloodorange,Common Orange,Acidless orange,----
+	String DrinksFlavour;
+	String Ingradients;
+	String DrinksFlavour; 
 }
 class PinappleJuice extends Juice{
-		PinappleType pinappleType; // Smooth Cayenne', 'Red Spanish', 'Queen', 'Abacaxi',----
-		String DrinksFlavour;
-		String Ingradients;
-	    String DrinksFlavour; 
+	PinappleType pinappleType; // Smooth Cayenne', 'Red Spanish', 'Queen', 'Abacaxi',----
+	String DrinksFlavour;
+	String Ingradients;
+    String DrinksFlavour; 
 }
 class SweetlimeJuice extends Juice{
 	    SweetlimeType sweetlimeType; // key lime,rangpur,Limequat
@@ -36,7 +36,10 @@ class SweetlimeJuice extends Juice{
 	    String DrinksFlavour; 
 }
 class Tea extends SoftDrinks{
-	
+	 TeaType teatype; // Hot,cold,normal
+	 List <TeaFlavour> teaFlavour; //green tea,black tea,normal tea
+}
+class TeaFlavour{
 	
 }
 class Coffee extends SoftDrinks{
