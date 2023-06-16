793b68c194ca9d8a8f3b12bcd9004e73ba451c01
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-16 12:04:10 +0530

    Inheritance example : Drinks

---
+++ JAVA/programs/Drinks.java
@@ -0,0 +1,49 @@
+class Drinks{
+	String Taste;
+	String Color;
+	string Consistancy;
+}
+class SoftDrinks extends Drinks {
+	SoftDrinkType softDrinkType; //soda,normal
+	SoftdrinkTempreture softdrinkTempreture; //normal,ice
+}
+class Juice extends SoftDrinks{
+	String JuiceType; //fresh,preserve
+    String Ingradients; // sugar, black salt, chat masala---
+}
+class MangoJuice extends Juice{
+	    MangoType mangoType; //Alfanso mango,Dashehri mango,Ratnagiri mango,kesar mango,----
+		String DrinksFlavour;
+		String Ingradients;
+	    String DrinksFlavour; 
+}
+class OrangeJuice extends Juice{
+	    OrangeType orangeType; //Nagpuri Orange,bloodorange,Common Orange,Acidless orange,----
+		String DrinksFlavour;
+		String Ingradients;
+	    String DrinksFlavour; 
+}
+class PinappleJuice extends Juice{
+		PinappleType pinappleType; // Smooth Cayenne', 'Red Spanish', 'Queen', 'Abacaxi',----
+		String DrinksFlavour;
+		String Ingradients;
+	    String DrinksFlavour; 
+}
+class SweetlimeJuice extends Juice{
+	    SweetlimeType sweetlimeType; // key lime,rangpur,Limequat
+		String DrinksFlavour;
+		String Ingradients;
+	    String DrinksFlavour; 
+}
+class Tea extends SoftDrinks{
+	
+	
+}
+class Coffee extends SoftDrinks{
+	
+	
+}
+
+class HardDrinks extends Drinks{
+	
+}
