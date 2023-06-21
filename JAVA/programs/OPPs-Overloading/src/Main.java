930f829b0b74fdc681561e53a67b4fdccc548804
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-21 11:47:27 +0530

    Exampples of Method Overloading

--- JAVA/programs/OPPs-Overloading/src/Main.java
+++ JAVA/programs/OPPs-Overloading/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-//   ----------Addition of 2-number and 3-numbers by using Methode Overloading--------------
+//   ----------(Example-1) Addition of 2-number and 3-numbers by using Methode Overloading--------------
         OverloadingAddition add=new OverloadingAddition();
-        System.out.println("Addition : ");
+        System.out.println("--------(Example-1)---------- Addition :---------- ");
         System.out.println("Addition of 2-number : ");
         int sum=add.addition(5,6);
         System.out.println(sum);
         System.out.println("Addition of 3-number : ");
         int sum1= add.addition(5,6,8);
         System.out.println(sum1);
-        //   ----------Substraction of 2-number and 3-numbers by using Methode Overloading--------------
+        //   ----------(Example-2)Substraction of 2-number and 3-numbers by using Methode Overloading--------------
 
         OverloadingSubstraction sub=new OverloadingSubstraction();
-        System.out.println("Substraction : ");
+        System.out.println("-------(Example-2)-------- Substraction :---------- ");
         System.out.println("Substraction of 2-number : ");
         int minus=sub.substract(8,5);
         System.out.println(minus);
@@ -23,7 +23,7 @@ public class Main {
         //   ----------Multiplication of 2-number and 3-numbers by using Methode Overloading--------------
 
         OverloadingMultiplication multiplication=new OverloadingMultiplication();
-        System.out.println("Multiplication : ");
+        System.out.println("-----(Example-3)--------- Multiplication :------- ");
         System.out.println("Multiplication of 2-number : ");
         int mul=multiplication.multiple(8,5);
         System.out.println(mul);
@@ -34,7 +34,7 @@ public class Main {
         //   ----------Division of 2-number and 3-numbers by using Methode Overloading--------------
 
         OverloadingDivision divide=new OverloadingDivision();
-        System.out.println("Division : ");
+        System.out.println(" -------(Example-4)------ Division :--------- ");
         System.out.println("Division of 2-number : ");
         int div=divide.division(8,5);
         System.out.println(div);
@@ -46,7 +46,7 @@ public class Main {
         //   ----------Modulus of 2-number and 3-numbers by using Methode Overloading--------------
 
         OverloadingModulus modulus=new OverloadingModulus();
-        System.out.println("Modulus : ");
+        System.out.println("-----(Example-5)------ Modulus :------------ ");
         System.out.println("Modulus of 2-number : ");
         int mod=modulus.modulus(8,5);
         System.out.println(mod);
@@ -57,14 +57,14 @@ public class Main {
         //   ----------String conacatinate by overloading using Methode Overloading--------------
 
         ConcateString concateString=new ConcateString();
-        System.out.println("String-Integeger Value : ");
+        System.out.println("-----(Example-6)---- String-Integeger Value :------ ");
         concateString.integerValue(25);
         concateString.stringValue("Welcome to MKPITS");
 
         //   ----------Person Details by overloading using Methode Overloading--------------
 
         PersonDetails person=new PersonDetails();
-        System.out.println("-----------Person Details :---------- ");
+        System.out.println("----(Example-7)-------Person Details :---------- ");
         person.details("Bhagyashri Jiddewar","Female");
         person.details("Bhagyashri Panchalwar","Female",28);
 
@@ -73,14 +73,14 @@ public class Main {
         //   ----------Person Details by overloading using Methode Overloading--------------
 
         Parants parant=new Parants();
-        System.out.println("----------parant Details:--------- ");
+        System.out.println("----(Example-8)------parant Details:--------- ");
         parant.property("Jiddewar",500000);
         parant.property("Bhagyashri","Jiddewar",600000,"Kind/Humble");
 
         //   ----------Perimeter of rectangle and semi-circle by overloading using Methode Overloading--------------
 
         Parimeter parimeter=new Parimeter();
-        System.out.println("----------Perimeter of rectangle and semicircle:--------- ");
+        System.out.println("-----(Example-9)-----Perimeter of rectangle and semicircle:--------- ");
         double perimeterSemicircle=parimeter.perimeter(5);
         double perimeterRectangle =parimeter.perimeter(6,3.14);
         System.out.println("Perimeter of Semicircle= "+perimeterSemicircle);
@@ -89,7 +89,7 @@ public class Main {
         //   ----------Volume of square and Sphere by overloading using Methode Overloading--------------
 
         Volume volumeOf=new Volume();
-        System.out.println("----------Volume of square and Sphere :--------- ");
+        System.out.println("-----(Example-10)-----Volume of square and Sphere :--------- ");
        double volumeOfSquare= volumeOf.volume(5);
        double volumeOfSphere= volumeOf.volume(3.2);
        System.out.println("Volume of square= "+volumeOfSquare);
