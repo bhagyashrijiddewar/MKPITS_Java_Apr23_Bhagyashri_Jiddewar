3d97ccaf38b58f2bd33f1d562e62e19d51799c98
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-06-24 15:41:48 +0530

    Loops in java

---
+++ JAVA/Loops/src/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args){
+        for(int number=0;number<=100;number = number - 1000){
+            System.out.println(number);
+
+        }
+
+    }
+}
