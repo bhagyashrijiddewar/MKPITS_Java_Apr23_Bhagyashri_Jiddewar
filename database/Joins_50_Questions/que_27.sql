9b1ce47c4f154f5c4559ce14deb9d24c2289bbcb
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-09-29 18:07:42 +0530

    sql joins queries from 21 to 31

---
+++ database/Joins_50_Questions/que_27.sql
@@ -0,0 +1,13 @@
+
+ 
+ -- (27) Get the names of customers who have rented the film titled "Murder Antitrust".
+    select* from customer
+	select* from rental 
+    select* from inventory
+    select* from film
+    
+	select first_name,last_name from customer join rental
+    on rental.customer_id=customer.customer_id join inventory
+    on inventory.inventory_id=rental.inventory_id join film 
+    on film.film_id=inventory.film_id where title="Murder Antitrust"
+ 
--- database/Joins_50_Questions/que_7.sql
+++ database/Joins_50_Questions/que_7.sql
@@ -2,7 +2,8 @@
 select*from film  
 select*from inventory 
 select * from rental     
+
 select title,rental_duration from film join inventory on film.film_id=inventory.film_id join 
 rental on rental.inventory_id=inventory.inventory_id
- where rental_date='2005-05-15'
+ where date(rental_date)='2005-05-15'
  
