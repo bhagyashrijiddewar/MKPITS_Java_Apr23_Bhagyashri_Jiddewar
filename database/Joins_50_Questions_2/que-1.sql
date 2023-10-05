df18c648156546926c123f9d4a699f6c3f9587b7
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-10-05 12:03:23 +0530

    Database query joins

---
+++ database/Joins_50_Questions_2/que-1.sql
@@ -0,0 +1,15 @@
+
+ use sakila
+ -- (1) Get the names of customers who have rented the film titled "Golden Groove".
+
+   select* from customer
+   select* from rental 
+   select*from inventory 
+   select*from actor
+   
+  select first_name,last_name from customer join rental on customer.customer_id=rental.customer_id 
+  join inventory on inventory.inventory_id=rental.inventory_id
+  join film on film.film_id=inventory.film_id 
+  where title="Golden Groove"
+ 
+
