17b4fa2bdf48af01929810220296c7381405e0e1
Bhagyashri Jiddewar <bhagyashrijiddewar17@gmail.com>
2023-10-10 18:29:46 +0530

    javascript

--- javascript/New_Js_Code/result.js
+++ javascript/New_Js_Code/result.js
@@ -17,7 +17,7 @@ function UserInput(marks){
      document.write("Fail");
 }
 
-// ----**User define input from front end **---
+// ----**static array value**---
 
 function myfun(){
     const cities=["nagpur","pune","mumbai"];
@@ -27,11 +27,54 @@ function myfun(){
     }
 }
 
-// ----**User define input from front end **---
+// ----**If user does not enter the value**----
 
 function validateValue(number){
     if(number.length==0)
         alert("Please enter value");
     return false;
-    
 }
+
+// ----**Sum of n Natural number **---
+
+function naturalNumber(number){
+    let sum=0;
+    for(var i=number;i>1;i--){
+        sum=sum+i;
+    }
+    document.write("Sum of n natural number = "+sum);
+}
+
+// ----**Number is a prime or not **---
+
+function primeNumber(number){
+   let isPrime=true;
+    if(number===1){
+        document.write("It is a composite number");
+    }
+    else if(number>1){
+        for(var i=2;i<number;i++){
+            if(number%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime)
+            document.write("Number is a prime.");
+        else
+            document.write("Number is not prime.");
+        }
+    }
+
+ //----** Reverse a String Using for Loop**----
+
+ function reverseString(str){
+
+    let newString="";
+    for(let i=str.length-1;i>=0;i--){
+        newString+=str[i];
+    }
+    document.write(newString);
+ }
+
+    
